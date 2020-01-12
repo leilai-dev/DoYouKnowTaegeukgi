6961e842eeaa9c07ebf24e93de9e16196279770c
leilai-dev <leilai.dev@gmail.com>
2020-01-12 17:05:26 +0900

    IAP를 위한 세팅 화면 구성

--- src/components/RNIap.js
+++ src/components/RNIap.js
@@ -3,11 +3,32 @@ import * as RNIap from "react-native-iap"
 import { Platform } from 'react-native';
 
 // Purchase items
-const itemSkus = Platform.select({
+export const itemSkus = Platform.select({
   // ios: [
   //   "INSERT YOUR PURCHASE ID IOS"
   // ],
   android: [
-    "android.doyouknowtaegeukgi.remove_ad" // 별도 상수로 빼야하나?
+    "android.doyouknowtaegeukgi.remove_ad", // 별도 상수로 빼야하나?
+    "android.test.purchased"
   ]
 });
+
+export const initConnection = async () => {
+  try {
+    return result = await RNIap.initConnection();
+  } catch (error) {
+    console.log(error.code, error.message);
+  }
+}
+
+export const getItems = async () => {
+  try {
+    // return products = await RNIap.getProducts(itemSkus);
+    const products = await RNIap.getProducts(itemSkus);
+    console.log('Products', products, itemSkus);
+    return products;
+  } catch (error) {
+    console.log(error.code, error.message);
+  }
+}
+
